8a1f14723766bc94dde4abc8cee7309583ed452d
Daniele Prevedello <prevedello@altervista.org>
2020-04-10 23:49:43 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+
+/******************************************************************************
+Classico programma introduttivo che permette di scrivere un messaggio di 
+saluto sulla console dell'utente.
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+
+    return 0;
+}
